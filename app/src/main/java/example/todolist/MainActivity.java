065b4b3ac4d1962670bdc051e3fde87fe0502a80
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2024-11-15 20:58:20 -0300

    basic project structure

--- app/src/main/java/example/todolist/MainActivity.java
+++ app/src/main/java/example/todolist/MainActivity.java
@@ -1,24 +1,40 @@
 package example.todolist;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        List<String> todoList = new ArrayList<>();
+        todoList.add("Estudiar para PEP");
+        todoList.add("Avanzar codigo proyecto");
+        todoList.add("Ir al supermercado");
+        todoList.add("Hacer Ejercicio");
+        todoList.add("Ordenar escritorio");
+        todoList.add("Entrevista técnica");
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todoList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedItem = todoList.get(position);
+            Intent intent = new Intent(MainActivity.this, ToDoDetailActivity.class);
+            intent.putExtra("TODO_ITEM", selectedItem);
+            startActivity(intent);
         });
     }
 }
